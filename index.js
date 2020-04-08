7c7fdb86f12355789b5e03339208b7a03ec40b9d
GRajesh1198 <50822370+GRajesh1198@users.noreply.github.com>
2020-04-08 15:58:43 +0530

    3.3: Phonebook backend step3

--- index.js
+++ index.js
@@ -37,6 +37,15 @@ app.get('/info',(req,res)=>{
         <p>${new Date()}
     `)
 })
+app.get('/api/persons/:id',(req,res)=>{
+    const id=Number(req.params.id)
+    const person=persons.find(person=>person.id===id)
+    if(person){
+        res.json(person)
+    }else{
+        res.status(404).end()
+    }
+})
 const PORT=3000
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
