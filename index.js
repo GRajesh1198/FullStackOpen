a2cc306ba4b6b20b744db4e201945f1e1ac00389
GRajesh1198 <50822370+GRajesh1198@users.noreply.github.com>
2020-04-08 16:10:26 +0530

    3.4: Phonebook backend step4

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express=require('express')
 const app=express()
 app.use(express.json())
 
-const persons=[
+let persons=[
     {
         name:"Arto Hellas",
         number:"040-123456",
         id:1
     },
     {
-        name:"Ada :Lovelace",
+        name:"Ada-Lovelace",
         number:39-44-53253523,
         id:2,
     },
@@ -45,6 +45,11 @@ app.get('/api/persons/:id',(req,res)=>{
     }else{
         res.status(404).end()
     }
+})
+app.delete('/api/persons/:id',(req,res)=>{
+    const id=Number(req.params.id)
+    persons=persons.filter(p=>p.id!==id)
+    res.status(204).end()
 })
 const PORT=3000
 app.listen(PORT)
